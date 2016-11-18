2b07cccdc315ea6fbb37dda3893192b05ea8dbf5
SrinidhiRaghavan <ss5145@columbia.edu>
2016-11-18 13:13:22 -0500

    config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+#Modify the following Twitter Details before executing
+TWITTER_ACCESS = "This_is_my_Twitter_Access_Key"
+TWITTER_SECRET = "This_is_my_Twitter_Secret_Key"
+TWITTER_CON_ACCESS = "This_is_the_Configuration_Key_of_Twitter_Access"
+TWITTER_CON_SECRET = "This_is_the_Configuration_Key_of_Twitter_Secret"
+
+#Modify the following AWS Details before executing 
+AWS_ACCESS_KEY="MY_AWS_ACCESS_KEY"
+AWS_SECRET_KEY="MY_AWS_SECRET_KEY"
+AWS_REGION="MY_AWS_REGION"
+
+#Modify the ES_Port and ES_Host as per your needs
+AWS_ES_PORT  = 1
+AWS_ES_HOST = "MY ES HOST" 
+AWS_ES_INDEX = 'tweets'
+AWS_ES_TYPE = 'tweets'
+
+#Modify GMAP_Key with your actual key 
+GMAP_KEY = 'MY GMAP KEY'
+
+#These are the categories used for filtering the tweets
+FILTERS = ['sports', 'music', 'politics', 'business', 'entertainment', 'health', 'weather', 'art', 'india', 'fun']
