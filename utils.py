a1eda585a72bebb52aa414d6d0d23d56d857adfb
SrinidhiRaghavan <ss5145@columbia.edu>
2016-11-18 13:00:53 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 def get_category(tweet):
 	final_cat = []
+	#FILTERS stores the user-defined categories which can be chosen 
 	FILTERS = ['sports', 'politics', 'business', 'music', 'tech', 'health', 'finance', 'india', 'books', 'celebrity']
 	for category in FILTERS:
 		if category in tweet:
